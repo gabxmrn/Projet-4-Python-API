43bce24f9b60195f2e363103434e5bae1d89fb2e
gabxmrn <149685872+gabxmrn@users.noreply.github.com>
2024-04-09 18:30:40 +0200

    clean

--- Code/Main.py
+++ Code/Main.py
@@ -3,11 +3,5 @@ from API_Global import Application
 # Instancier l'application
 app = Application()
 
-# On centre les boutons dans la fenêtre
-app.update_idletasks()
-largeur_fenetre = app.winfo_width()
-for widget in app.winfo_children():
-    widget.pack_configure(anchor='n', padx=(largeur_fenetre - widget.winfo_reqwidth()) // 2)
-
 # Lancer l'application
 app.mainloop()
