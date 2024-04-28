82acf3d5ff094c1e4551609c9501efef81d812df
gabxmrn <149685872+gabxmrn@users.noreply.github.com>
2024-04-28 16:25:15 +0200

    Update Page_HTML2.py

--- Code/API_2/Page_HTML2.py
+++ Code/API_2/Page_HTML2.py
@@ -1,3 +1,4 @@
+import sys
 from Toolboxs.html_toolbox import HTMLToolBox
 
 
@@ -12,7 +13,7 @@ def construction_API2() -> str:
     return API2.generate_page()
 
 
-class API_2_ContentGenerator: 
+class API_2_ContentGenerator:
     """
     Classe pour générer le contenu de la page API2.
     """
@@ -22,7 +23,7 @@ class API_2_ContentGenerator:
         Initialise une nouvelle instance de la classe API_2_ContentGenerator.
         """
         super().__init__()
-    
+
     def generate_page(self) -> str:
         """
         Génère le contenu HTML de la page API2.
@@ -31,82 +32,169 @@ class API_2_ContentGenerator:
             str: Le contenu HTML de la page API2.
         """
 
-        # Saut de ligne
-        line_break = HTMLToolBox.generate_line_break()
+        # Line break
+        lb = HTMLToolBox.generate_line_break()
 
-        # Titre du projet
-        project_name = 'Création d’Analytics de Trading Personnalisés via API'
-        title = HTMLToolBox.generate_title(project_name)
+        # Titre de la page
+        title = HTMLToolBox.generate_title('Création d’Analytics de Trading Personnalisés via API')
 
-        # Descriptif du projet
+        # Description de l'API
         project_description = """API qui permet à l'utilisateur de créer et de recevoir des analytics de trading personnalisés. 
                             Cet API fournit une large gamme d'indicateurs d'analyse technique appliqués aux marchés de crypto-actifs.
                             Les utilisateurs peuvent également soumettre et appliquer leurs propres fonctions d'analyse Python3 personalisées aux données historiques et à celles en temps réel. """
         description = HTMLToolBox.generate_paragraph(project_description)
 
-        # Sélection de la fonction
-        function_title = HTMLToolBox.generate_input_selection("Sélection de la fonction à utiliser :")
-        remarque = HTMLToolBox.generate_paragraph("Pour tester le code, veuillez entrer : fnc1, fnc2 ou fnc3.")
-        fnc = HTMLToolBox.generate_str_input_box("fnc","")
-    
-        # Sélection des paramètres de marché
-        parameter_title = HTMLToolBox.generate_input_selection("Sélection des paramètres de marché :")
+        # Sections de la page
+        websocket_section = self.generate_websocket_section()
+        indicators_section = self.generate_indicators_section()
+        
+        # Génération de la page HTML
+        return title + description + lb + \
+            websocket_section + lb + lb + \
+            indicators_section + lb + lb
+
+    def generate_websocket_section(self) -> str:
+        """
+        Génère le contenu HTML de la première section de la page API2.
+
+        Returns:
+            str: Le contenu de la section Websockets de la page API2.
+        """
+
+        # Contenu pour la section Websocket
+        title = HTMLToolBox.generate_subtitle("Websocket : Données en direct")
+        description1 = HTMLToolBox.generate_paragraph(
+            "Cette section permet de recevoir des données en temps réel via WebSocket.")
 
         # Liste des tickers de crypto-actifs (USDT quote-asset)
-        ticker_input_box = HTMLToolBox.generate_tick_input_box("tickers","Ticker des crypto-actifs (USDT quote-asset) :")
+        ticker_input_box = HTMLToolBox.generate_tick_input_box("tickers",
+                                                               "Ticker des crypto-actifs (USDT quote-asset) :")
 
-        # Type de données à récupérer
+        description2 = HTMLToolBox.generate_paragraph(
+            "Exemple : pour le BTC, veuillez écrire BTCUSDT.")
+
+        # Fréquence
+        freq_input_box = HTMLToolBox.generate_str_input_box("f",
+                                                            "Fréquence entre les points de données (ex. 1s, 1min, 1h, 1d, 1w, 1m ou 1y) :")
+        
+        # Boîte de saisie pour la durée de la période
+        period_input_box = HTMLToolBox.generate_str_input_box("period", "Durée de la période de calcul :")
+
+        # Indicateur à sélectionner
+        indicators = ["SMA","EMA", "MACD", "RSI", "BBands"]
+        dropdown_indicators = "<label for='type' style='font-family: Helvetica, sans-serif; font-weight: bold; display: block;'>Indicateur à calculer : </label>"
+        dropdown_indicators += HTMLToolBox.generate_dropdown_menu("indic", indicators, False)
+
+        # Boutons pour les analyses techniques via WebSocket
+        button_code = """
+        <div style="display: flex; justify-content: center;">
+            <button id="BtnWebsocket" style="background-color: lightblue; padding: 10px 20px; font-size: 16px;">Données réelles - Calcul de l'indicateur :</button>
+        </div>
+        <div id="result"></div>
+        """
+
+        # Script pour gérer les clics des boutons
+        script = """
+        <script>
+            document.getElementById("BtnWebsocket").addEventListener("click", function() {
+
+            var tickers = document.getElementById("tickers").value;
+            var f = document.getElementById("f").value;
+            var n = parseInt(document.getElementById("period").value);
+            var indic = Array.from(document.getElementById("indic").selectedOptions).map(option => option.value);
+
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "/run_code_api2", true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.onload = function () {
+                if (xhr.status === 200) {
+                    document.getElementById("result").innerHTML = xhr.responseText;
+                }
+            };
+            xhr.send(JSON.stringify({hist_rt:"rt", fnc: indic, tickers: tickers, freq: f, n:n}));
+        });
+
+        </script>
+        """
+
+        return title + description1 + \
+            ticker_input_box + description2 + \
+            freq_input_box + period_input_box + dropdown_indicators + \
+            button_code + script
+
+    def generate_indicators_section(self) -> str:
+        """
+        Génère le contenu HTML de la seconde section de la page API2.
+
+        Returns:
+            str: Le contenu de la section Données Historiques de la page API2.
+        """
+
+        # Contenu pour la section des indicateurs spécifiés
+        title = HTMLToolBox.generate_subtitle("Données historiques - Calcul d'indicateur")
+
+        # Descriptif
+        description = "Cette section permet de calculer des indicteurs à l'aide de données historiques."
+
+        # Indicateur existant ou personnalisé
+        indic_title = HTMLToolBox.generate_input_selection("Entre l'indicateur à calculer :")
+        rq = HTMLToolBox.generate_paragraph("""Les indicteurs implémentés sont : SMA, EMA, MACD, RSI, BBANDS.
+                                            Si vous souhaitez calculer un indicateur personnalisé, veuillez entrer le nom de la fonction.
+                                            Pour tester une fonction personnalisée, veuillez entrer test. """)
+        fnc = HTMLToolBox.generate_str_input_box("fnc","")
+
+        # Paramètres de marchés
+
+        ## Liste des tickers de crypto-actifs (USDT quote-asset)
+        ticker_input_box = HTMLToolBox.generate_tick_input_box("tick","Ticker des crypto-actifs (USDT quote-asset) :")
+
+        ## Type de données à récupérer
         data_type = ["Open","High", "Low", "Close", "Volume"]
         dropdown_data_type = "<label for='type' style='font-family: Helvetica, sans-serif; font-weight: bold; display: block;'>Type de données à récupérer : </label>"
         dropdown_data_type += HTMLToolBox.generate_dropdown_menu("datatype", data_type, True)
 
-        # Boite de saisie des dates
+        ## Boite de saisie des dates
         date1_input_box = HTMLToolBox.generate_date_input_box("startDate", "Date de début :")
         date2_input_box = HTMLToolBox.generate_date_input_box("endDate", "Date de fin :")
 
-        # Fréquence
+        ## Fréquence
         freq_input_box = HTMLToolBox.generate_str_input_box("freq","Fréquence entre les points de données (ex. 1s, 1min, 1h, 1d, 1w, 1m ou 1y) :")
 
-        #  Message à l'utilisateur
-        msg = HTMLToolBox.generate_paragraph("Le bouton ci-dessous exécute le code. Si jamais vous n'avez pas rentré les paramètres appropriés, un message d'erreur s'affichera dans la console." +
-                                             "De la même manière, si rien ne s'affiche sur la page HTML, veuillez regarder dans la console.")
-
-        # Bouton pour lancer le code
+        # Boutons pour les indicateurs
         button_code = """
         <div style="display: flex; justify-content: center;">
-            <button id="BtnAPI2" style="background-color: lightgreen; padding: 10px 20px; font-size: 16px;">Importation des paramètres :</button>
+            <button id="BtnHisto" style="background-color: lightblue; padding: 10px 20px; font-size: 16px;">Données historiques - Calcul de l'indicateur :</button>
         </div>
         <div id="result"></div>
         """
-        
-        # Récupération des données entrées par l'utilisateur
+
+        # Code du bouton
         script = """
         <script>
-            document.getElementById("BtnAPI2").addEventListener("click", function() {
-                var startDate = document.getElementById("startDate").value;
-                var endDate = document.getElementById("endDate").value;
-                var freq = document.getElementById("freq").value;
-                var dataType = Array.from(document.getElementById("datatype").selectedOptions).map(option => option.value);
-                var tick = document.getElementById("tickers").value;
-                var fnc = document.getElementById("fnc").value;
-
-                var xhr = new XMLHttpRequest();
-                xhr.open("POST", "/run_code_api2", true);
-                xhr.setRequestHeader('Content-Type', 'application/json');
-                xhr.onload = function () {
-                    if (xhr.status === 200) {
-                        document.getElementById("result").innerHTML = xhr.responseText;
-                    }
-                };
-                xhr.send(JSON.stringify({fnc:fnc, startDate: startDate, endDate: endDate, dataType:dataType, freq: freq, tickers:tick}));
-            });
+        document.getElementById("BtnHisto").addEventListener("click", function() {
+                
+            var startDate = document.getElementById("startDate").value;
+            var endDate = document.getElementById("endDate").value;
+            var freq = document.getElementById("freq").value;
+            var dataType = Array.from(document.getElementById("datatype").selectedOptions).map(option => option.value);
+            var tick = document.getElementById("tick").value;
+            var fnc = document.getElementById("fnc").value;
+        
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "/run_code_api2", true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.onload = function () {
+                if (xhr.status === 200) {
+                    document.getElementById("result").innerHTML = xhr.responseText;
+                }
+            };
+            xhr.send(JSON.stringify({hist_rt:"hist", fnc:fnc, startDate: startDate, endDate: endDate, dataType:dataType, freq: freq, tickers:tick}));
+        });
         </script>
         """
-        
-        # Génération de la page HTML
+
         return title + description + \
-            function_title + remarque + fnc + parameter_title + \
+            indic_title + rq + fnc + \
             ticker_input_box + dropdown_data_type + \
-            date1_input_box + date2_input_box + freq_input_box + msg + \
-            line_break + button_code + script + line_break + line_break
-    
+            date1_input_box + date2_input_box + freq_input_box + \
+            button_code + script
